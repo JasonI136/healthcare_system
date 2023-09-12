0e988dd2664308fa67ef656b0470cdefd9488093
Jason Immanuel <jason.immanuel136@gmail.com>
2023-09-12 23:57:21 +1000

    added files for doctor implementation

---
+++ model/dto/AppointmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthcare_system.model.dto
+{
+    // Data Transfer Object for Appointments
+    public class AppointmentDTO
+    {
+        public DoctorDTO doctor { get; set; }
+        public PatientDTO patient { get; set; }
+        public DateTime date { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ model/dto/DoctorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthcare_system.model.dto
+{
+    // Data Transfer Object for Users
+    public class DoctorDTO
+    {
+        public UserDTO user { get; set; }
+
+    }
+}
+
