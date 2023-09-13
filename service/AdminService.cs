1fa14fb135059162e8171f941313a167b2049c2b
Jason Immanuel <63634201+JasonI136@users.noreply.github.com>
2023-09-13 19:13:22 +1000

    check doctor and patient implementation

--- service/AdminService.cs
+++ service/AdminService.cs
@@ -50,6 +50,9 @@ namespace healthcare_system.service
             }
         }
 
+        /* Option 1: List All Doctors
+        * Lists a shorthand version of every doctor contained in the system to the console
+        */
         public void listAllDoctor(int signedInId)
         {
             Console.Clear();
@@ -83,16 +86,67 @@ namespace healthcare_system.service
                 Console.WriteLine("No user found with the ID " + signedInId);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to the menu.");
             Console.ReadLine();
         }
 
 
-
+        /* Option 2: Check Doctor Details
+        * Prompts the user for an ID and prints the details of the doctor whose ID it belongs to, to the console line by line
+        */
         public void checkDoctorDetails(int signedInId)
         {
+            Console.Clear();
+            menuService.DisplayHeader("Check Doctor Details:");
 
+            List<UserDTO> userList = userService.LoadUserList();
+
+            UserDTO user = userList.Find(u => u.UserId == signedInId);
+            if (user != null)
+            {
+                Console.WriteLine("Enter the Doctor ID to retrieve details (or press ENTER to exit):");
+                string input = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    return;
+                }
+                if (int.TryParse(input, out int doctorId))
+                {
+                    UserDTO doctor = userList.Find(u => u.UserId == doctorId && string.Equals(u.Role, "Doctor", StringComparison.OrdinalIgnoreCase));
+                    if (doctor != null)
+                    {
+                        Console.WriteLine("{0,-10} {1,-10} {2,-20} {3,-20} {4,-30}", "UserId", "Role", "First Name", "Last Name", "Email");
+                        Console.WriteLine(new string('-', 90));
+                        Console.WriteLine("{0,-10} {1,-10} {2,-20} {3,-20} {4,-30}", doctor.UserId, doctor.Role, doctor.FirstName, doctor.LastName, doctor.Email);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No doctor found with the ID " + doctorId);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ID entered. Please enter a valid number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No user found with the ID " + signedInId);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to the menu.");
+            Console.ReadKey();
         }
 
+
+        /* Option 3: List All Patients
+        * Lists a shorthand version of every patient contained in the system to the console
+        */
         public void listAllPatients(int signedInId)
         {
             Console.Clear();
@@ -126,14 +180,61 @@ namespace healthcare_system.service
                 Console.WriteLine("No user found with the ID " + signedInId);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to the menu.");
             Console.ReadLine();
         }
 
-
+        /* Option 4: Check Patient Details
+        * Prompts the user for an ID and prints the details of the Patient whose ID it belongs to, to the console line by line
+        */
         public void checkPatientDetails(int signedInId)
         {
+            Console.Clear();
+            menuService.DisplayHeader("Check Patient Details:");
 
+            List<UserDTO> userList = userService.LoadUserList();
+
+            UserDTO user = userList.Find(u => u.UserId == signedInId);
+            if (user != null)
+            {
+                Console.WriteLine("Enter the Patient ID to retrieve details (or press ENTER to exit):");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    return;
+                }
+                if (int.TryParse(input, out int patientId))
+                {
+                    UserDTO patient = userList.Find(u => u.UserId == patientId && string.Equals(u.Role, "Patient", StringComparison.OrdinalIgnoreCase));
+                    if (patient != null)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("{0,-10} {1,-10} {2,-20} {3,-20} {4,-30}", "UserId", "Role", "First Name", "Last Name", "Email");
+                        Console.WriteLine(new string('-', 90));
+                        Console.WriteLine("{0,-10} {1,-10} {2,-20} {3,-20} {4,-30}", patient.UserId, patient.Role, patient.FirstName, patient.LastName, patient.Email);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No patient found with the ID " + patientId);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ID entered. Please enter a valid number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No user found with the ID " + signedInId);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to the menu.");
+            Console.ReadKey();
         }
+
 
         public void addDoctor(int signedInId)
         {
